cf79370551b0ab1f20ef2388ed79edceeee5efb1
tvloc02 <iamtvloc02@gmail.com>
2025-10-01 09:45:09 +0700

    standard

--- backend/models/Standard.js
+++ backend/models/Standard.js
@@ -51,12 +51,6 @@ const standardSchema = new mongoose.Schema({
         min: [1, 'Thứ tự phải lớn hơn 0']
     },
 
-    weight: {
-        type: Number,
-        min: [0, 'Trọng số không được âm'],
-        max: [100, 'Trọng số không được vượt quá 100']
-    },
-
     objectives: {
         type: String,
         trim: true,
