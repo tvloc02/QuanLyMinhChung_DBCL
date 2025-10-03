6a28e91515dd5bd29abbc26106ee6cfa0df505d6
tvloc02 <iamtvloc02@gmail.com>
2025-10-03 16:30:17 +0700

    importBath

--- backend/routes/report/importBatch.js
+++ backend/routes/report/importBatch.js
@@ -1,26 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const importBatchController = require('../../controllers/report/ImportBatchController');
-const auth = require('../../middleware/auth');
 
-router.post('/', auth, importBatchController.createBatch);
-
-router.get('/', auth, importBatchController.getUserBatches);
-
-router.get('/:id', auth, importBatchController.getBatchById);
-
-router.get('/:id/errors', auth, importBatchController.getBatchErrors);
-
-router.get('/stats/summary', auth, importBatchController.getBatchStats);
-
-router.put('/:id/start', auth, importBatchController.startBatch);
-
-router.put('/:id/complete', auth, importBatchController.completeBatch);
-
-router.put('/:id/fail', auth, importBatchController.failBatch);
-
-router.put('/:id/cancel', auth, importBatchController.cancelBatch);
-
-router.delete('/cleanup/old', auth, importBatchController.cleanupBatches);
+router.post('/', importBatchController.createBatch);
+router.get('/', importBatchController.getUserBatches);
+router.get('/:id', importBatchController.getBatchById);
+router.get('/:id/errors', importBatchController.getBatchErrors);
+router.get('/stats/summary', importBatchController.getBatchStats);
+router.put('/:id/start', importBatchController.startBatch);
+router.put('/:id/complete', importBatchController.completeBatch);
+router.put('/:id/fail', importBatchController.failBatch);
+router.put('/:id/cancel', importBatchController.cancelBatch);
+router.delete('/cleanup/old', importBatchController.cleanupBatches);
 
 module.exports = router;
