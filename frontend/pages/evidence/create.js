5ab7a6cd46a64e9c926b54223ea0f2486d53e2e2
tvloc02 <iamtvloc02@gmail.com>
2025-10-04 23:42:41 +0700

    evidence AddEvidenceManual.js

--- frontend/pages/evidence/create.js
+++ frontend/pages/evidence/create.js
@@ -37,7 +37,7 @@ export default function AddEvidencePage() {
 
     return (
         <Layout
-            title="Thêm minh chứng"
+            title=""
             breadcrumbItems={breadcrumbItems}
         >
             <div className="space-y-6">
