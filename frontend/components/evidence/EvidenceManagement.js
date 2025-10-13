e389ebf4ba7ca3cce840b353d1876bb0410ce254
tvloc02 <iamtvloc02@gmail.com>
2025-10-13 13:44:04 +0700

    frontend/components/evidence/EvidenceManagement.js

--- frontend/components/evidence/EvidenceManagement.js
+++ frontend/components/evidence/EvidenceManagement.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { apiMethods } from '../../services/api'
-import { ActionButton } from '../../components/ActionButtons'
+import { ActionButton } from '../ActionButtons'
 import toast from 'react-hot-toast'
 import {
     Plus,
