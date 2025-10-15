d2ae55e622d891f1d4572b9f1a585e50a11591fc
tvloc02 <iamtvloc02@gmail.com>
2025-10-15 11:57:40 +0700

    app.js

--- backend/app.js
+++ backend/app.js
@@ -38,6 +38,8 @@ const userGroupRoutes = require('./routes/user/userGroup');
 const systemRoutes = require('./routes/system/system');
 const importBatchRoutes = require('./routes/report/importBatch');
 const permissionRoutes = require('./routes/user/permission');
+//import aiChatRoutes from './routes/aiChat.js';
+// app.use('/api/ai-chat', aiChatRoutes);
 app.use('/api/user-groups', userGroupRoutes);
 app.use('/api/permissions', permissionRoutes);
 app.use('/api/import-batches', importBatchRoutes);
