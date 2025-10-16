af85229c9428c0da7d937e7737e206c7ba09eb56
tvloc02 <iamtvloc02@gmail.com>
2025-10-16 17:18:50 +0700

    ProgramList.js

--- frontend/components/structure/ProgramList.js
+++ frontend/components/structure/ProgramList.js
@@ -6,7 +6,7 @@ import { formatDate } from '../../utils/helpers'
 import * as XLSX from 'xlsx'
 import ImportExcelModal from './ImportExcelModal'
 import ProgramModal from './ProgramModal'
-import { ActionButton } from '../../components/ActionButtons' // Giả định đã có
+import { ActionButton } from '../ActionButtons'
 
 export default function ProgramList() {
     const [programs, setPrograms] = useState([])
